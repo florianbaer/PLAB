759e849fc206e742e989519630ee873a4f48ff17
Florian Bär <binaryfr3ak@gmail.com>
2017-09-24 12:16:16 +0200

    Implmeent first cucumber test

---
+++ PLAB-MyCode/src/test/java/com/baerdev/backofen/BackofenSteps.java
@@ -0,0 +1,31 @@
+package com.baerdev.backofen;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+
+@CucumberOptions(features = "src/test/resources/")
+
+public class BackofenSteps {
+    Backofen backofen = null;
+
+
+
+    @Given("^a instance of backofen$")
+    public void a_instance_of_backofen() throws Throwable {
+        backofen = null;
+    }
+
+    @When("^i instanciate it$")
+    public void i_instanciate_it() throws Throwable {
+        backofen = new Backofen("Backofen 2000", new Heizelement[]{new Heizelement(new HeizelementTyp("Röhre"))}, new Uhr(), new Tuere());
+    }
+
+    @Then("^the instance is not null$")
+    public void the_instance_is_not_null() throws Throwable {
+        Assert.assertNotNull(backofen);
+    }
+}
---
+++ PLAB-MyCode/src/test/resources/com/baerdev/backofen/Backofen.feature
@@ -0,0 +1,6 @@
+Feature: This is the Backofen Feature
+
+  Scenario: This is a dummy scenario
+    Given a instance of backofen
+    When i instanciate it
+    Then the instance is not null
