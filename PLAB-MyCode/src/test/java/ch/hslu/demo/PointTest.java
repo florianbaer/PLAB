15494e5b0c9e5e81707f22be22349e5309feeb26
Florian Bär <binaryfr3ak@gmail.com>
2017-09-24 11:41:03 +0200

    Import mockito to tests 	# Please enter the commit message for your changes. Lines starting 	# with '#' will be ignored, and an empty message aborts the commit. 	# On branch master 	# Your branch is up-to-date with 'origin/master'. 	# 	# Changes to be committed: 	#	modified:   PLAB-MyCode/PLAB-MyCode.iml 	#	modified:   PLAB-MyCode/pom.xml 	#	modified:   PLAB-MyCode/src/test/java/ch/hslu/demo/PointTest.java 	#

--- PLAB-MyCode/src/test/java/ch/hslu/demo/PointTest.java
+++ PLAB-MyCode/src/test/java/ch/hslu/demo/PointTest.java
@@ -19,6 +19,8 @@ import nl.jqno.equalsverifier.EqualsVerifier;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
 import org.junit.Test;
